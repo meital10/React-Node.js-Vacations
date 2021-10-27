df83d35efb3ae5f88aa351dd43ad86e225d58532
meital <aharonim8@gmail.com>
2021-10-27 20:48:29 +0300

    build changes

---
+++ server/build/precache-manifest.fa0e4c86e17646a8f8fff1ed121fcd5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df59dca9b0fdb817cd04d7756f104d7d",
+    "url": "/React-Node.js-Vacations/index.html"
+  },
+  {
+    "revision": "269e8038f83d0e9ce65a",
+    "url": "/React-Node.js-Vacations/static/css/main.3365a173.chunk.css"
+  },
+  {
+    "revision": "ebd6e09106e12e233b16",
+    "url": "/React-Node.js-Vacations/static/js/2.04e66d17.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/React-Node.js-Vacations/static/js/2.04e66d17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "269e8038f83d0e9ce65a",
+    "url": "/React-Node.js-Vacations/static/js/main.d793ade1.chunk.js"
+  },
+  {
+    "revision": "369a347242cc08fb0f61",
+    "url": "/React-Node.js-Vacations/static/js/runtime-main.06f9842f.js"
+  }
+]);
