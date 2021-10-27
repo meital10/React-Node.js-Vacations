5b9301f023ab7eaf6472aa55c37e37b5488ca0e0
meital <aharonim8@gmail.com>
2021-10-27 20:28:49 +0300

    server changed

--- server/app.js
+++ server/app.js
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  // res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.use(bodyParser.urlencoded({ extended: true }));
